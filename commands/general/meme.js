d5cbd396e4bdcc0c15d0064a74d43d539b3b7d14
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-05 06:02:35 -0700

    Fixed the rng for reddit searc

--- commands/general/meme.js
+++ commands/general/meme.js
@@ -22,27 +22,41 @@ module.exports = {
     run: async (client, message, args) => {
         let msg = await message.channel.send("Generating...")
         
-        message.channel.send({//channel.send
-            files: [{ attachment: await imgSend() }]
-        }).then(async msg => {
-            try {
-                await msg.react('581795233067433985');
-                await msg.react('595108561831460875');
-                await msg.react('598068228220256266')
-            } catch (error) {
-
-                console.error("failed to add emoji")
-            }
-        }).then(message.delete(2000) ).then(msg.delete(2000))
+       try {
+         message.channel.send({//channel.send
+             files: [{ attachment: await imgSend() }]
+         }).then(async msg => {
+             try {
+                 await msg.react('581795233067433985');
+                 await msg.react('595108561831460875');
+                 await msg.react('598068228220256266')
+             } catch (error) {
+ 
+                 console.error("failed to add emoji")
+             }
+         }).then(message.delete(2000) ).then(msg.delete(2000))
+       } catch (error) {
+        console.log(error)
+       }
     }
 }
 async function  imgSend(){
     
     return new Promise(resolve =>{
-        var rng = Math.floor(Math.random() * 3) + 0;
+        var rng = Math.floor(Math.random() * 8) + 0;
         var url = new Array;
         console.log("url rng is: " + rng)
-        url = ["https://www.reddit.com/r/Animemes/top/.json", "https://www.reddit.com/r/Animemes/.json", "https://reddit.com/r/Animemes/top/.json?t=week", "https://reddit.com/r/Animemes/top/.json?t=month"]
+        url = [
+            "https://www.reddit.com/r/Animemes/top/.json", 
+            "https://www.reddit.com/r/Animemes/.json", 
+            "https://reddit.com/r/Animemes/top/.json?t=week", 
+            "https://reddit.com/r/Animemes/top/.json?t=month",
+            "https://reddit.com/r/Animemes/top/.json?t=year",
+
+            "https://reddit.com/r/Animemes/top/.json?t=today",
+            "https://reddit.com/r/Animemes/top/.json?t=all",
+            "https://www.reddit.com/r/Animemes/new/.json"
+        ]
     
         fetch(url[rng])
             .then(function (response) {
@@ -62,13 +76,13 @@ async function  imgSend(){
                         imgUrl[i] = childData.url;
                     };
                     
-                    var rng = Math.floor(Math.random() * children.length - 3 + 1) + 3;
+                    var rng = Math.floor(Math.random() * (children.length-1) - 0) + 0;
                     console.log("children number = " + children.length)
                     console.log("Meme rng is" + rng)
                     if(rng ==25) {rng=0}
                     var imgFile = imgUrl[rng];
                     if (imgFile.startsWith("https://v.")) {
-                        imgFile = imgFile + "/DASH_96.mp4"
+                        imgFile = imgFile + "/DASH_1080.mp4"
                     }
                     // if (imgFile != undefined) {
     
