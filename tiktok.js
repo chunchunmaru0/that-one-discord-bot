981f910787697d89f9a3f6cf825b5d159544025a
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-06 00:51:36 -0700

    Added second Tiktok Downloader

--- tiktok.js
+++ tiktok.js
@@ -7,7 +7,7 @@ module.exports = {
 
         //await message.channel.send("I checked and verified the channel.")
         if (!message.content.startsWith('https://www.tik')) {
-            console.log("No video File")
+            //console.log("No video File")
             return;
         }
 
@@ -16,40 +16,48 @@ module.exports = {
         //console.log(content)
         let linkArr = (content.slice().trim().split(/ +/g))
         let link = linkArr.shift();
-        console.log(link)
+        console.log("Link to Clip:", link)
 
 
         try {
             ttdl.getInfo(link)
                 .then(async (result) => {
-                    console.log(link)
+                    console.log("Inside regular:", link)
                     console.log(result);
                     if (result.sucess == false) return
                     //console.log("here");
                     let videoLink;
                     let name;
                     if (result.video.url.wm == undefined) {
-                        console.log(link)
-                        // v1(link).then(async data => {
-                        //     // Do something with the data
-                        //     videoLink = data.video.noWatermark;
-                        //     name = data.title;
-                        //     //console.log("Version 1", data)
-                        //     console.log(videoLink, name)
-                        //     const file = new AttachmentBuilder(videoLink)
-                        //         .setName(`${name}.mp4`)
-                        //         .setDescription('TikTok Video')
+
+                        try {
+                            console.log("Inside Undefined Error:", link)
+                            let msg2 = await message.channel.send("Oof! Trying Another Method!!")
+                            v1(link).then(async data => {
+                                // Do something with the data
+                                videoLink = data.video.noWatermark;
+                                name = data.title;
+                                //console.log("Version 1", data)
+                                console.log(videoLink, name)
+                                const file = new AttachmentBuilder(videoLink)
+                                    .setName(`${name}.mp4`)
+                                    .setDescription('TikTok Video')
 
 
-                        //     console.log(file)
-                        //     await message.reply({ files: [file] })
-                        //         .then(setTimeout(() => { message.suppressEmbeds(true) }, 5000))
-                        //         .then(setTimeout(() => {msg.delete()},5000));
-                        // });
+                                console.log(file)
+                                await message.reply({ files: [file] })
+                                    .then(setTimeout(() => { message.suppressEmbeds(true) }, 5000))
+                                    .then(setTimeout(() => { msg.delete() }, 5000))
+                                    .then(setTimeout(() => { msg2.delete() }, 5000));
+                            });
+                        } catch (error) {
+                            console.log(error)
+                        }
+
                     } else {
                         videoLink = result.video.url.no_wm;
                         name = result.author.name;
-                        console.log("From TVD",videoLink, name)
+                        console.log("From TVD", videoLink, name)
                         const file = new AttachmentBuilder(videoLink)
                             .setName(`${name}.mp4`)
                             .setDescription('TikTok Video')
@@ -58,7 +66,7 @@ module.exports = {
                         console.log(file)
                         await message.reply({ files: [file] })
                             .then(setTimeout(() => { message.suppressEmbeds(true) }, 5000))
-                            .then(setTimeout(() => {msg.delete()},5000));
+                            .then(setTimeout(() => { msg.delete() }, 5000));
 
                     }
 
@@ -69,7 +77,7 @@ module.exports = {
                 })
         } catch (error) {
             console.log(error)
-            await message.reply("Welp! Got an Error Dude!")
+            await message.reply("Welp! Got an Error Dude!").then(setTimeout(() => { msg.delete() }, 5000));
         }
     }
 }
