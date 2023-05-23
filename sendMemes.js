f35998a3037139f6f2f75a91aa881bae765fe0d3
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-23 15:31:02 -0700

    Uses the Reddit Newpost fetcher class to get the new memes

---
+++ sendMemes.js
@@ -0,0 +1,32 @@
+const RedditCheckPost = require('./reddit-wrapper/RedditCheckPost.js');
+
+module.exports = {
+
+    async sendMemes(discordClient) {
+        const channel_id = '594964222677614592'
+        const channel = await discordClient.channels.fetch(channel_id);
+
+        let values = await getNewMemesReddit()
+        if (values.length !== 0) {
+            for (const val of values) {
+                try {
+                    await channel.send(val.url);
+                } catch (error) {       
+                        console.log(error)
+                        //await channel.send("Something Went Wrong!")
+                        //https://files.catbox.moe/6udng2.JPG                
+                }
+            }
+        }
+    }
+}
+
+async function getNewMemesReddit() {
+    let getMemes = new RedditCheckPost('new', 'Memes')
+    try {
+        let MemesRes = await getMemes.meme();
+        return (MemesRes)
+    } catch (error) {
+
+    }
+}
