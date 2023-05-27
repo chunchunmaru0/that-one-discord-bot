b74cdfe1860b53a46800bfcee3830fc73fa2126d
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-26 23:28:00 -0700

    added Test function to download from redgifs

--- pinterest.js
+++ pinterest.js
@@ -1,7 +1,7 @@
-const { AttachmentBuilder, EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { AttachmentBuilder} = require('discord.js');
 const axios = require('axios');
 const cheerio = require('cheerio');
-
+const { downloadRedgifsHD, downloadRedgifsSD, delVid } = require('./utils/redgifs.js')
 module.exports = {
     async pinterest(client, message) {
 
@@ -43,8 +43,8 @@ module.exports = {
 
                     console.log(file)
                     await message.reply({ files: [file] })
-                    .then(setTimeout(() => { message.suppressEmbeds(true) }, 5000))
-                    .then(setTimeout(() => { msg.delete() }, 5000))
+                        .then(setTimeout(() => { message.suppressEmbeds(true) }, 5000))
+                        .then(setTimeout(() => { msg.delete() }, 5000))
                 })
                 .catch(function (response) {
                     //handle error
@@ -52,7 +52,41 @@ module.exports = {
                 });
         }
 
+        // if (message.content.startsWith('https://www.redgifs')) {
+        //     console.log(message.content)
+        //     let vidLoc = (await downloadRedgifsHD(message.content, './vids'))
+        //     let vidName = vidLoc.split('/').pop()
+        //     const file = new AttachmentBuilder(vidLoc)
+        //         .setName(`${vidName}`)
+        //         .setDescription(message.content)
 
+        //     console.log(file)
+
+        //     try {
+        //         await message.reply({ files: [file] }).then(
+        //             setTimeout(() => {
+        //                 delVid(vidLoc);
+        //             }, 5000))
+        //     } catch (error) {
+        //         if (error.code === 40005) {
+        //             console.log("File Big trying SD")
+        //             let vidLoc = (await downloadRedgifsSD(message.content, './vids'))
+        //             let vidName = vidLoc.split('/').pop()
+        //             const file = new AttachmentBuilder(vidLoc)
+        //                 .setName(`${vidName}`)
+        //                 .setDescription(message.content)
+
+        //             console.log(file)
+        //             await message.reply({ files: [file] }).then(
+        //                 setTimeout(() => {
+        //                     delVid(vidLoc);
+        //                 }, 5000)).catch((err => { console.log(err) }))
+        //         } else {
+        //             console.log(error)
+        //         }
+        //     }
+
+        // }
 
     }
 
